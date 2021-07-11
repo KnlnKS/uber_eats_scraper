66260cebe306ad4ea9ea2f9d6ed18163c5b165b2
KnlnKS <soulkks@hotmail.ca>
2021-07-11 00:43:18 -0400

    ⚡️ Parallelize

--- restaurant_scraper.py
+++ restaurant_scraper.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
 import os
 
+from joblib import Parallel, delayed
+
+
+def get_urls(cat_url):
+    try:
+        driver = webdriver.Chrome(executable_path="chromedriver_win32/chromedriver.exe")
+        driver.get(cat_url)
+        temp_urls = driver.find_element_by_xpath("/html/body/div/div/main/div[5]").find_elements_by_tag_name("a")
+        for url in temp_urls:
+            out_file = open("temp_urls.txt", "a")
+            out_file.write(str(url.get_attribute("href")) + "\n")
+    except:
+        print("Skipped " + cat_url)
+
 
 def scrape_restaurants(base_url, location):
     driver = webdriver.Chrome(executable_path="chromedriver_win32/chromedriver.exe")
@@ -9,17 +23,7 @@ def scrape_restaurants(base_url, location):
     categories = driver.find_element_by_xpath("/html/body/div/div/main/div[2]/div[3]"). \
         text.replace(" ", "-").lower().splitlines()
 
-    for cat in categories:
-        try:
-            driver.get(base_url + location + "/" + cat)
-            temp_urls = driver.find_element_by_xpath("/html/body/div/div/main/div[5]").find_elements_by_tag_name("a")
-            for url in temp_urls:
-                out_file = open("temp_urls.txt", "a")
-                out_file.write(str(url.get_attribute("href")) + "\n")
-        except:
-            print("Skipped " + cat)
-
-    out_file.close()
+    Parallel(n_jobs=5)(delayed(get_urls)(base_url + location + "/" + cat) for cat in categories)
 
     lines_seen = set()  # holds lines already seen
     out_file = open(location + "_restaurant_urls.txt", "w+")
