cb3e6bb4a9f1f11651c458dd06baa50f6d2be551
KnlnKS <soulkks@hotmail.ca>
2021-12-11 22:36:22 -0500

    🎨 Format Code

--- spiders/categories.py
+++ spiders/categories.py
@@ -6,7 +6,7 @@ import re
 
 class CategoriesSpider(scrapy.Spider):
     name = 'categories'
-
+    
     def __init__(self, city='', **kwargs):
         self.start_urls = ['https://www.ubereats.com/ca/category/' + city]
         super().__init__(**kwargs)
@@ -14,15 +14,15 @@ class CategoriesSpider(scrapy.Spider):
     def parse(self, response):
         sel = Selector(response)
         content = sel.xpath('//*[@id="__REACT_QUERY_STATE__"]/text()').getall()[0].replace("\\u0022", '"')
-        fixedContent = re.sub(r',"queryHash":"\[(.*?)\]"', '', content)
-        queryData = json.loads(fixedContent)
-        categoryData = queryData["queries"][0]["state"]["data"]["navigationLinks"][0]["subNav"]
+        fixed_content = re.sub(r',"queryHash":"\[(.*?)\]"', '', content)
+        query_data = json.loads(fixed_content)
+        category_data = query_data["queries"][0]["state"]["data"]["navigationLinks"][0]["subNav"]
 
-        for i in range(len(categoryData)):
-            categoryData[i]["text"] = re.sub(r' Delivery in [^\\]*', '', categoryData[i]["text"])
-            categoryData[i].pop("imgSrc")
-            categoryData[i].pop("rank")
-            categoryData[i].pop("iconUrl")
-            categoryData[i].pop("backgroundColor")
-            categoryData[i].pop("label")
-            categoryData[i].pop("isAnchor")
+        for i in range(len(category_data)):
+            category_data[i]["text"] = re.sub(r' Delivery in [^\\]*', '', category_data[i]["text"])
+            category_data[i].pop("imgSrc")
+            category_data[i].pop("rank")
+            category_data[i].pop("iconUrl")
+            category_data[i].pop("backgroundColor")
+            category_data[i].pop("label")
+            category_data[i].pop("isAnchor")
